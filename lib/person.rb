a13801c4dc1f84eca11fbe4176a1ba70ee69a30a
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-01 01:30:35 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,12 +4,12 @@ def Person
     @name = name
   end
 
-  # def name= (name)
-  #   @name = name
-  # end
-  #
-  # def name
-  #   @name
-  # end
+  def name= (name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
 
 end
